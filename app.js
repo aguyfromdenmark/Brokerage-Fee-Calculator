1e138375e89922c5573911553805c3ca77c8f3a9
Andreas Kristensen <ankrweb@gmail.com>
2017-02-01 12:49:48 +0100

    Added localstorage support. Checked Account type and Market will now be remebered.

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+var lsAccountType = localStorage.getItem('lsAccountType');
+var lsMarket = localStorage.getItem('lsMarket');
+
+if (lsAccountType !== null && lsMarket !== null) {
+    $('input[name=accountType][value='+lsAccountType+']').prop('checked',true);
+    $('input[name=market][value='+lsMarket+']').prop('checked',true);    
+}
+
+if (typeof(Storage) !== 'undefined') {
+    var accountTypeRadio = $('input[name=accountType]');
+    var marketRadio = $('input[name=market]');
+
+    accountTypeRadio.change(function(){
+        var selectedAccountType = $(this).val();  
+        localStorage.setItem('lsAccountType', selectedAccountType);
+    });
+
+    marketRadio.change(function(){
+        var selectedMarket = $(this).val();
+        localStorage.setItem('lsMarket',selectedMarket);
+    });
+}
+
 $('#tradeCalculateButton').on('click', function () {
 
     var accountType = $('input[name=accountType]:checked').val();
